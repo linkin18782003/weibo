4879fcc0981509d0c3762f12813a3c574b333892
chenrui <chenrui@chenrui.name>
2019-11-14 09:17:48 +0000

    基础页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models\User;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
-
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
      *
